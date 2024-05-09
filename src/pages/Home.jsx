d04fa505d16cdbc0793b671b3f98a9ead166d989
Khodja <138789644+Abdurahmon-2004@users.noreply.github.com>
2024-05-09 15:06:28 +0500

    rasmni oldim

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import Beach from "../assets/beach.jpg";
+// import Beach from "../assets/beach.jpg";
 
 export default function Home() {
   return (
     <div>
       <div className="justify-center absolut flex ">
-        <img src={Beach} alt="" className="   " />
+        {/* <img src={Beach} alt="" className="   " /> */}
       </div>
       <h2>
         Enjoy <span>Reserved</span> Overhead bin space
