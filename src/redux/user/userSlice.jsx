d4d99049cca554e7cd986e60e096e81833e6e656
AyubxonUz <yuldoshevv7@gmail.com>
2024-05-13 13:55:43 +0500

    added localstorage

--- src/redux/user/userSlice.jsx
+++ src/redux/user/userSlice.jsx
@@ -3,11 +3,12 @@ import {createSlice} from "@reduxjs/toolkit"
 export const userSlice = createSlice({
   name: "user",
   initialState: {
-    userData: null,
+    userData: JSON.parse(localStorage.getItem("userData")) || null,
   },
   reducers: {
     setUserData: (state, {payload}) => {
       state.userData = payload
+      localStorage.setItem("userData", JSON.stringify(payload))
     },
   },
 })
